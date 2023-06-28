03c6112c1fb6740b8ccaf89cd8a17ff9a1744c93
Aron Lecznar <arondlecznar@gmail.com>
2023-06-27 23:33:02 -0400

    Add my first file

---
+++ code/rock_paper_scissors.py
@@ -0,0 +1,12 @@
+
+
+
+my_choice = input('Make selection from either rock, paper, or scissors')
+
+if my_choice != 'rock' or 'paper' or 'scissors':
+    print("Please select from the listed options")
+
+print(f'You selected: {my_choice}')
+
+
+
